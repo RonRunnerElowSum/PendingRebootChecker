c1389c239b24cf23eb9b4e44678db1d95ea7c501
RonRunnerElowSum <97925118+RonRunnerElowSum@users.noreply.github.com>
2022-01-19 12:57:06 -0500

    Update PRC Installer.ps1

--- PRC Installer.ps1
+++ PRC Installer.ps1
@@ -1,15 +1,18 @@
-$PRCps1URL = "https://raw.githubusercontent.com/RonRunnerElowSum/PendingRebootChecker/Prod-Branch/PRC.ps1"
-$PRCLaunchString = "Invoke-WebRequest -URI $PRCps1URL -UseBasicParsing | Invoke-Expression; PunchIt"
+$TaskName = "(MSP) Pending Reboot Checker"
+$PSFileURL = "'https://raw.githubusercontent.com/RonRunnerElowSum/PendingRebootChecker/Prod-Branch/PRC.ps1'"
+
+$ScheduledTaskCmd = "Invoke-WebRequest -URI $PSFileURL -UseBasicParsing | Invoke-Expression; PunchIt"
+$ScheduledTaskArg = "-WindowStyle Hidden -Command `"& {$ScheduledTaskCmd}`""
 
 function CreateSchedTask () {
-    $Action = New-ScheduledTaskAction -Execute 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe' -Argument "-WindowStyle Hidden $PRCLaunchString"
+    $Action = New-ScheduledTaskAction -Execute 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe' -Argument $ScheduledTaskArg
     $Trigger = @()
     $Trigger += New-ScheduledTaskTrigger -AtLogon
     $Trigger += New-ScheduledTaskTrigger -Daily -At 12pm
     $Settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -WakeToRun -StartWhenAvailable
-    $Principal = New-ScheduledTaskPrincipal -UserId $Env:Username -LogonType Interactive
-    Register-ScheduledTask -Action $Action -Trigger $Trigger -Settings $Settings -Principal $Principal -TaskName "(MSP) Pending Reboot Checker" -Description "Monitors for pending reboots." | Out-Null
-    if(!(Get-ScheduledTask -TaskName "(MSP) Pending Reboot Checker" -ErrorAction SilentlyContinue)){
+    $Principal = New-ScheduledTaskPrincipal -GroupId "BUILTIN\Users"
+    Register-ScheduledTask -Action $Action -Trigger $Trigger -Settings $Settings -Principal $Principal -TaskName $TaskName -Description "Monitors for pending reboots." | Out-Null
+    if(!(Get-ScheduledTask -TaskName $TaskName -ErrorAction SilentlyContinue)){
         Write-Warning "Failed to create schedule task...exiting..."
         EXIT
     }
@@ -20,7 +23,7 @@ function CreateSchedTask () {
 }
 
 function PunchIt () {
-    if(!(Get-ScheduledTask -TaskName "(MSP) Pending Reboot Checker" -ErrorAction SilentlyContinue)){
+    if(!(Get-ScheduledTask -TaskName $TaskName -ErrorAction SilentlyContinue)){
         CreateSchedTask
     }
     else{
